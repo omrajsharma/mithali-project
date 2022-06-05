7e169bc74d3097e5cbd20332d622e9d9450a524c
omrajsharma <omraj7413@gmail.com>
2022-06-05 19:22:36 +0530

    tail wind css setup

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-red-200">
+    <h1 class="text-3xl font-bold underline bg-red">
+      Hello world!
+    </h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
