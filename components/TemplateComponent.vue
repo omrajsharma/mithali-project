4fc07b41737ff7df57ceeb8e8ae562350eb08c74
omrajsharma <omraj7413@gmail.com>
2022-06-06 20:12:21 +0530

    Template component added

---
+++ components/TemplateComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-screen h-screen flex items-center justify-center">
+        <div class="m-4">
+            <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md w-full">
+                hey
+            </div>
+        </div>
+    </div>
+</template>
