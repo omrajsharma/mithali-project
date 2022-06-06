3fd0bcf1171923c8ca8d2456a26c7caaed27b040
omrajsharma <omraj7413@gmail.com>
2022-06-06 22:34:30 +0530

    moodSequence added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-screen h-screen bg-cyan-500">
+    <!-- INTRODUCTION -->
     <div v-if="appIntro.display">
       <UserDetails v-if="appIntro.step == 1" :login="IntroStepInc"/>
       <MoodSelect v-if="appIntro.step == 2" v-on:setMoodNumber="setMoodNum" />
@@ -14,7 +15,19 @@
         <span>Mood List : </span>
         {{currentMoodList}}
       </div>
+      <div>
+        <span>Mood sequence : </span>
+        {{moodSequence}}
+      </div>
     </div>
+    <!-- END INTRODUCTION -->
+
+
+    <!-- ARTICLES -->
+    <div v-if="articles.displayArticle">
+      articles
+    </div>
+    hey
   </div>
 </template>
 
@@ -32,7 +45,6 @@ export default {
     MoodSelector,
     CurrentMood,
     FirstNote,
-
   },
 
   // DATA
@@ -41,10 +53,51 @@ export default {
       appIntro : {
         display: true,
         step: 1,
-        moodNumber: 0,
       },
+      moodSequence : null,
       currentMoodList: [],
+      articles: {
+        displayArticle: false,
+        // currentMood: this.moodSequence,
+        happy: [
+          {
+            content: [
+              "JAKARTA - The world's COVID-19 deaths surpassed the five million mark on Friday, according to Reuters data. It took the world more than a year to reach the first 2.5 million COVID-19 deaths during the pandemic, but the second 2.5 million was achieved in just under eight months. An average of 8,000 deaths were reported daily worldwide in the past week, or about five deaths per minute. More than half of the global death toll in that week's average came from the United States, Russia, Brazil, Mexico and India. The total death toll in the US on Friday crossed 700,000, the highest in the world.",
+              "While COVID-19 cases and hospitalizations in the US are trending down, the country's health authorities are bracing for a spike in winter when people are more active indoors. On the same day Russia reported 887 deaths, the country's highest daily figure for a year pandemic. South America is the region with the highest mortality rate in the world, accounting for 21 percent of all reported deaths. ",
+              "The region is followed by North America and Eastern Europe, which each account for more than 14 percent of all deaths, according to a Reuters analysis. , the death toll in India -- one of the countries hardest hit by the Delta variant -- has dropped dramatically from an average of 4,000 per day to less than 300 when the vaccination program was launched. Delta is now the world's dominant variant of the coronavirus and has been found in 187 of the 194 member countries of the World Health Organization (WHO). ",
+              "These variants have shown different vaccination rates high levels between rich and poor countries. World attention in recent days has focused on efforts to share vaccines with poor countries, where many people have not received the first dose. At the same time, people in wealthier countries are starting to be given booster doses. More than half of the world's population has not received a single dose of the COVID-19 vaccine, according to Our World in Data. ",
+              "WHO said this week that for the first time the COVAX program will distribute doses only to countries with the lowest vaccination rates. The vaccine sharing program since January has allocated most of the vaccine doses to more than 140 recipient countries in proportion to the population. assistant Director-General of WHO, in a recording of last week's conference presentation uploaded to the WHO's official website."
+            ],
+            quiz: {
+              question: '',
+              option: [
+                {
+                  text: '789',
+                  correct: false,
+                },
+                {
+                  text: '800',
+                  correct: false,
+                },
+                {
+                  text: '889',
+                  correct: false,
+                },
+                {
+                  text: '887',
+                  correct: true,
+                },
+              ],
+            }
+          }
+        ],
+        sad: [
 
+        ],
+        neutral:[
+
+        ],
+      },
     };
   },
 
@@ -58,7 +111,8 @@ export default {
     },
     setMoodNum(value){
       console.log('inside parent')
-      this.appIntro.moodNumber = value
+      // this.appIntro.moodNumber = value
+      this.moodSequence = value;
       this.appIntro.step++;
     },
     setMoodList(value){
@@ -68,6 +122,7 @@ export default {
     introFinish(){
       this.appIntro.display = false;
       this.appIntro.step++;
+      this.articles.displayArticle = true;
     },
   },
 
@@ -79,6 +134,20 @@ export default {
     loginStatus() {
       return this.step == 3;
     }
-  }
+  },
+
+  // WATCH
+  watch: {
+    step(value) {
+      console.log('step changed to ' + value);
+    },
+  },
+
+  // MOUNTED
+  mounted() {
+    console.log(this.articles);
+    console.log(this.appIntro);
+  },
+
 }
 </script>
